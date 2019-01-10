848050b01430ccfd0b0409c8796b9193cfdc3220
Matej Ferenc <ferenc.matej@gmail.com>
2019-01-10 16:51:48 +0100

    fixed deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:dlabs/devops-infra-rails-example.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
